e94671b0b41d84ce3e021e2d5e77c2ff2e8ae57c
shimonori <8index103000@gmail.com>
2020-07-17 20:38:39 +0900

    Initial Commit

---
+++ TimerAPP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimerAPP
+//
+//  Created by 下里浩昇 on 2020/07/17.
+//  Copyright © 2020 shimo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
